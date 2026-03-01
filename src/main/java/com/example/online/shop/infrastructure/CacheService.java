08dd8fce2e386c53237f0e47be15975a8e68d3af
Dmitriy G <dgsubscr23@gmail.com>
2026-03-01 16:37:02 +0200

    Add gradlew

--- src/main/java/com/example/online/shop/infrastructure/CacheService.java
+++ src/main/java/com/example/online/shop/infrastructure/CacheService.java
@@ -1,7 +1,4 @@
 package com.example.online.shop.infrastructure;
 
-import com.example.online.shop.payment.service.PaymentService;
-
 public class CacheService {
-    PaymentService paymentService = new PaymentService();
 }
