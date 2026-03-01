3ecac62bf0fd19505eb04dbda2a216355005aef9
Dmitriy G <dgsubscr23@gmail.com>
2026-03-01 16:54:02 +0200

    Fix tests

--- src/main/java/com/example/online/shop/order/service/OrderService.java
+++ src/main/java/com/example/online/shop/order/service/OrderService.java
@@ -1,9 +1,8 @@
 package com.example.online.shop.order.service;
 
-import com.example.online.shop.payment.service.PaymentService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
-   PaymentService paymentService = new PaymentService();
+
 }
